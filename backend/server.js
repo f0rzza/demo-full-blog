c66feae60317fd358ca7aee09aa09b361473495c
Florent <forzza.dev@gmail.com>
2025-11-23 17:43:12 +0100

    chore(backend): ajoute le suivi des requêtes HTTP en erreur via le package morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import { categoryRoutes, postRoutes, userRoutes } from './src/routes/index.js';
 
 // Load custom ENV file
@@ -10,6 +11,10 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined', {
+  // Log only error responses.
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
 
 // Initialize the routes
 app.use("/categories", categoryRoutes);
