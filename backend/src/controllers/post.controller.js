05c4607996f71f9c039cd8e42338cba27572c867
Florent <forzza.dev@gmail.com>
2025-11-23 17:47:16 +0100

    fix(backend): ajoute la gestion des erreurs pour les requêtes API des articles

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -2,53 +2,76 @@ import postService from '../services/post.service.js';
 
 async function getAllPosts(req, res) {
   const posts = await postService.findAllPosts();
-  res.json( posts );
+  res.json(posts);
 }
 
 async function getPostById(req, res) {
+  // Get post from current ID.
   const postId = parseInt(req.params.id);
   const post = await postService.findPostById(postId);
-  res.json( post );
+
+  if (!post) {
+    return res.status(404).json({ error: `Post with id: ${postId} not found.` });
+  }
+
+  res.json(post);
 }
 
 async function createPost(req, res) {
-  const { title, content, authorId } = req.body;
-  const parsedAuthorId = parseInt(authorId);
-  // TODO: use 'Zod' package or eq to get validated data.
-  const post = await postService.createNewPost({ title, content, authorId: parsedAuthorId });
-  res.json( post );
+  try {
+    const { title, content, authorId } = req.body;
+    const parsedAuthorId = parseInt(authorId);
+    // TODO: use 'Zod' package or eq to get validated data.
+
+    // Create new post.
+    const post = await postService.createNewPost({ title, content, authorId: parsedAuthorId });
+
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: `An error has occurred. No post were created.` });
+  }
 }
 
 async function updatePostById(req, res) {
-  const postId = parseInt(req.params.id);
-  const { title, content, published, authorId } = req.body;
-  const isPublished = (published === 'true');
-  const parsedAuthorId = parseInt(authorId);
-  // TODO: use 'Zod' package or eq to get validated data.
+  try {
+    const postId = parseInt(req.params.id);
+    const { title, content, published, authorId } = req.body;
+    const isPublished = (published === 'true');
+    const parsedAuthorId = parseInt(authorId);
+    // TODO: use 'Zod' package or eq to get validated data.
 
-  const updatedPost = await postService.updatePostById(postId, {
-    title,
-    content,
-    published: isPublished,
-    authorId: parsedAuthorId,
-  });
+    // Update existing post.
+    const updatedPost = await postService.updatePostById(postId, {
+      title,
+      content,
+      published: isPublished,
+      authorId: parsedAuthorId,
+    });
 
-  if (!updatedPost) {
-    return res.status(404).json({ error: `Post with id: ${postId} not found. No post were updated.` });
+    if (!updatedPost) {
+      return res.status(404).json({ error: `Post with id: ${postId} not found. No post were updated.` });
+    }
+
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ error: `An error has occurred. No post were updated.` });
   }
-
-  res.json( updatedPost );
 }
 
 async function deletePostById(req, res) {
-  const postId = parseInt(req.params.id);
-  const deletedPost = await postService.deletePostById(postId);
+  try {
+    // Delete existing post.
+    const postId = parseInt(req.params.id);
+    const deletedPost = await postService.deletePostById(postId);
 
-  if (!deletedPost) {
-    return res.status(404).json({ error: `Post with id: ${postId} not found. No post were deleted.` });
+    if (!deletedPost) {
+      return res.status(404).json({ error: `Post with id: ${postId} not found. No post were deleted.` });
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: `An error has occurred. No post were deleted.` });
   }
-
-  res.sendStatus(200);
 }
 
 export default {
