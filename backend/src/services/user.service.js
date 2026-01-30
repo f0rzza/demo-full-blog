098e616059729d0afeeaf5a4530804270399859a
f0rzza <forzza.dev@gmail.com>
2026-01-30 13:20:06 +0100

    feat(backend): améliore la création de comptes utilisateur (vérification email/nom, hashage du password) (#2)
    * chore(backend): ajoute les dépendances liées à l'authentification
    
    * feat(backend): ajoute des fonctions de récupération d'utilisateurs
    
    * fix(backend): ajoute la vérification du nom et du mail avant la création d'un utilisateur
    
    * feat(backend): ajoute le hashage des mots de passe lors de la création de comptes

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,19 +1,28 @@
-import userRepository from "../repositories/user.repository.js";
+import HttpError from '../errors/HttpError.js';
+import userRepository from '../repositories/user.repository.js';
+import bcryptjs from 'bcryptjs';
 
-async function findAllCategories() {
+// Get a list of users
+async function findAllUsers() {
   const users = await userRepository.getAll();
   return users;
 }
 
+// Get a user by ID
 async function findUserById(id) {
   const user = await userRepository.getById(id);
   return user;
 }
 
+// Create a new user
 async function createNewUser({ username, email, password }) {
-  // TODO : check email and username
-  // TODO : generate hashed password
-  const user = await userRepository.create({ username, email, password });
+  // Check if username / email are available.
+  await checkEmailAndUsernameAvailability(username, email);
+  // Generate hashed password
+  const saltRounds = Number(process.env.PASSWORD_SALT) || 10;
+  const hashedPassword = await bcryptjs.hash(password, saltRounds);
+  // Create new user
+  const user = await userRepository.create({ username, email, password: hashedPassword });
   return user;
 }
 
@@ -37,9 +46,39 @@ async function deleteUserById(id) {
 }
 
 export default {
-  findAllCategories,
+  findAllUsers,
   findUserById,
   createNewUser,
   updateUserById,
   deleteUserById,
 };
+
+/* ---------- helpers privés ---------- */
+
+// Check if username / email are available.
+async function checkEmailAndUsernameAvailability(username, email) {
+  const existingUsers = await userRepository.getUserWithEmailOrUsername({
+    username,
+    email,
+  });
+
+  // If there are not existing users, username / email are available.
+  if (existingUsers.length === 0) {
+    return;
+  }
+
+  // If there are 2 existing users, both data are not available.
+  if (existingUsers.length === 2) {
+    throw new HttpError('Email and username already exists', 409);
+  }
+
+  // If there are only one existing user, check both data.
+  if (existingUsers[0].email === email) {
+    if (existingUsers[0].username === username) {
+      throw new HttpError('Email and username already exists', 409);
+    }
+    throw new HttpError('Email already exists', 409);
+  }
+
+  throw new HttpError('Username already exists', 409);
+}
