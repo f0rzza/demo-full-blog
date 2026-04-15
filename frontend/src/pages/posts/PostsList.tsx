3f44ded85428d24ecf953342bd7793f9cf217486
Florent <forzza.dev@gmail.com>
2026-04-15 15:57:48 +0200

    feat: ajoute les composants de page pour les articles

---
+++ frontend/src/pages/posts/PostsList.tsx
@@ -0,0 +1,7 @@
+export function PostsList() {
+  return (
+    <>
+      <h1>List of post</h1>
+    </>
+  );
+}
---
+++ frontend/src/pages/posts/index.ts
@@ -0,0 +1,6 @@
+import { PostCreate } from './PostCreate';
+import { PostDetails } from './PostDetails';
+import { PostEdit } from './PostEdit';
+import { PostsList } from './PostsList';
+
+export { PostsList, PostCreate, PostDetails, PostEdit };
