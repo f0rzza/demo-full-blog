624d55751c4769ffe6cbf0b67c6631b33c8d2306
Florent <forzza.dev@gmail.com>
2026-04-21 00:19:39 +0200

    feat: ajoute un nouveau layout pour l'éditeur des articles (admin)

--- frontend/src/layouts/index.ts
+++ frontend/src/layouts/index.ts
@@ -1,4 +1,5 @@
 import { BasicLayout } from './BasicLayout';
 import { DashboardLayout } from './content/DashboardLayout';
+import { PostEditorLayout } from './content/PostEditorLayout';
 
-export { BasicLayout, DashboardLayout };
+export { BasicLayout, DashboardLayout, PostEditorLayout };
