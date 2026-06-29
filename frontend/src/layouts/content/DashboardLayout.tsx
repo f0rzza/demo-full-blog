2f60ec5b124746a206a07e4331105d377ed61ff5
Florent <forzza.dev@gmail.com>
2026-06-29 20:15:49 +0200

    feat(drawer): affiche uniquement la version mobile de la sidebar sur certaines pages admin

--- frontend/src/layouts/content/DashboardLayout.tsx
+++ frontend/src/layouts/content/DashboardLayout.tsx
@@ -1,17 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { DashboardSidebar } from '../components/sidebars/DashboardSidebar';
 import { DashboardDrawer } from '@/features/posts/components/admin/forms/DashboardDrawer';
 
 export function DashboardLayout() {
+  const { pathname } = useLocation();
+  // For admin pages where the desktop version of the drawer is not possible, we display the mobile version.
+  const displayOnlyDrawer = ['/admin/posts/create', '/admin/categories'].includes(pathname);
+
   return (
     <div className="flex flex-1 max-w-[1600px] w-full mx-auto px-8 md:px-12 py-12 gap-12">
       {/* Desktop sidebar */}
-      <aside className="hidden lg:block">
-        <DashboardSidebar />
-      </aside>
+      {!displayOnlyDrawer && (
+        <aside className="hidden lg:block">
+          <DashboardSidebar />
+        </aside>
+      )}
 
-      {/* Mobile sidebar */}
-      <div className="block lg:hidden">
+      {/* Mobile sidebar or for specific pages */}
+      <div className={`block ${displayOnlyDrawer ? '' : 'lg:hidden'}`}>
         <DashboardDrawer>
           <DashboardSidebar />
         </DashboardDrawer>
